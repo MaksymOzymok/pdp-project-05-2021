f1b5ff30af8d0411eefc7bb93eb7c2e556ba5887
Maksym Ozymok <48139743+MaksymOzymok@users.noreply.github.com>
2021-05-09 19:28:52 +0300

    updated api base url

--- config/axios.ts
+++ config/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:4000/",
+  baseURL:
+    "https://my-json-server.typicode.com/MaksymOzymok/pdp-project-05-2021/db/",
 });
 
 export default axiosClient;
-
